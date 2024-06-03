686b2d9dd37b46d119dffc779572ad47e8c796b1
Elena <eof812@gmail.com>
2024-06-03 10:30:06 +0200

    ahora si esta bien

--- standcustom/standcustom/src/main/java/com/ElenaOrtega/standcustom/service/PartidaService.java
+++ standcustom/standcustom/src/main/java/com/ElenaOrtega/standcustom/service/PartidaService.java
@@ -1,7 +1,5 @@
 package com.ElenaOrtega.standcustom.service;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,24 +9,23 @@ import org.springframework.stereotype.Service;
 import com.ElenaOrtega.standcustom.entity.PartidaEntity;
 import com.ElenaOrtega.standcustom.repository.PartidaRepository;
 
-
 @Service
 public class PartidaService {
 
+    private final PartidaRepository partidaRepository;
+    private final SessionService oSessionService;
+
     @Autowired
-    private PartidaRepository partidaRepository;
-   
-    @Autowired
-    private SessionService oSessionService;
-    @Autowired 
-   
+    public PartidaService(PartidaRepository partidaRepository, SessionService oSessionService) {
+        this.partidaRepository = partidaRepository;
+        this.oSessionService = oSessionService;
+    }
+
     public PartidaEntity get(Long id) {
-       
         return partidaRepository.findById(id).orElse(null);
     }
 
     public Long create(PartidaEntity partidaEntity) {
-       
         partidaRepository.save(partidaEntity);
         return partidaEntity.getId();
     }
@@ -45,38 +42,35 @@ public class PartidaService {
     }
 
     public Page<PartidaEntity> getPage(Pageable pageable) {
-        
         return partidaRepository.findAll(pageable);
     }
 
     public PartidaEntity getOneRandom() {
-oSessionService.onlyAdmins();
+        oSessionService.onlyAdmins();
         Pageable oPageable = PageRequest.of((int) (Math.random() * partidaRepository.count()), 1);
         return partidaRepository.findAll(oPageable).getContent().get(0);
     }
-     public Long populate(Integer amount) {
-  oSessionService.onlyAdmins();
-    for (int i = 0; i < amount; i++) {
 
-        PartidaEntity partida = new PartidaEntity();
-       
-       partida.setFecha("18-02-2024");
-       
-     
-       partidaRepository.save(partida);
+    public Long populate(Integer amount) {
+        oSessionService.onlyAdmins();
+        for (int i = 0; i < amount; i++) {
+            PartidaEntity partida = new PartidaEntity();
+            partida.setFecha("18-02-2024");
+            partidaRepository.save(partida);
+        }
+        return amount.longValue();
     }
-    return amount.longValue();
-}
-public Long empty() {
-         oSessionService.onlyAdmins();
 
+    public Long empty() {
+        oSessionService.onlyAdmins();
         partidaRepository.deleteAll();
+        // Assuming resetAutoIncrement() and flush() are valid methods in your repository
         partidaRepository.resetAutoIncrement();
         partidaRepository.flush();
         return partidaRepository.count();
     }
+
     public Long getLastCreatedId() {
-      
         Pageable pageable = PageRequest.of(0, 1);
         Page<PartidaEntity> lastPartidaPage = partidaRepository.findAll(pageable);
         if (lastPartidaPage.hasContent()) {
@@ -85,5 +79,5 @@ public Long empty() {
         } else {
             return null;
         }
-}
+    }
 }
